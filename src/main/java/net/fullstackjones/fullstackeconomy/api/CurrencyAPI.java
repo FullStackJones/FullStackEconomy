7aa202ad1c81ee0860e0030906bee86c3f971ff8
FullStackJones <gregoryjones0498@gmail.com>
2025-03-24 14:11:27 +0000

    implementing unit tests

--- src/main/java/net/fullstackjones/fullstackeconomy/api/CurrencyAPI.java
+++ src/main/java/net/fullstackjones/fullstackeconomy/api/CurrencyAPI.java
@@ -3,41 +3,51 @@ package net.fullstackjones.fullstackeconomy.api;
 import net.fullstackjones.fullstackeconomy.items.CurrencyItem;
 import net.minecraft.world.item.Item;
 
-public class CurrencyAPI {
+public class CurrencyAPI implements ICurrencyAPI{
+    public CurrencyAPI() {
+    }
 
-    public static void CreateCurrency(){
+    @Override
+    public void CreateCurrency() {
         // create currency data
     }
 
-    public static void DeleteCurrency(){
+    @Override
+    public void DeleteCurrency() {
         // delete currency data
     }
 
-    public static CurrencyItem CreateCurrencyItem(){
+    @Override
+    public CurrencyItem CreateCurrencyItem() {
         // create currencyItem data
         return new CurrencyItem(new Item.Properties());
     }
 
-    public static void DeleteCurrencyItem(){
+    @Override
+    public void DeleteCurrencyItem() {
         // delete currencyItem data
     }
 
-    public static void SetCurrencyInCirculation(CurrencyItem currencyItem, String Currency){
+    @Override
+    public void SetCurrencyInCirculation(CurrencyItem currencyItem, String Currency) {
         // Update coins in circulation
     }
 
-    public static CurrencyItem[] GetCurrencyInCirculation(CurrencyItem currencyItem, String Currency){
+    @Override
+    public CurrencyItem[] GetCurrencyInCirculation(CurrencyItem currencyItem, String Currency) {
         return new CurrencyItem[]{};
     }
 
-    public static CurrencyItem[] GetSumOfCurrency(int value, int value2, String Currency){
+    @Override
+    public CurrencyItem[] GetSumOfCurrency(int value, int value2, String Currency) {
         int sumValue = value + value2;
         // get currency data by name
         // create currency array based off of the sumValue
         return new CurrencyItem[]{};
     }
 
-    public static CurrencyItem[] GetSubtractionOfCurrency(int value, int value2, String Currency){
+    @Override
+    public CurrencyItem[] GetSubtractionOfCurrency(int value, int value2, String Currency) {
         int subtractedValue = value - value2;
         // get currency data by name
         // create currency array based off of the subtractedValue
---
+++ src/main/java/net/fullstackjones/fullstackeconomy/data/EconomyData.java
@@ -0,0 +1,8 @@
+package net.fullstackjones.fullstackeconomy.data;
+
+import java.util.Dictionary;
+import java.util.UUID;
+
+public class EconomyData {
+    Dictionary<UUID, CurrencyData> _Currencies;
+}
